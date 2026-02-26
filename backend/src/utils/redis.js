fa24453b1405bb0bf543ad2c424a308ca235e10c
WebCasting28 <ansarisikkim786@gmail.com>
2026-02-26 20:32:12 +0530

    change in redis

--- backend/src/utils/redis.js
+++ backend/src/utils/redis.js
@@ -2,7 +2,7 @@ import ioredis from 'ioredis';
 
 const redis = new ioredis({
     host: process.env.REDIS_HOST,
-    port:  Number(process.env.REDIS_PORT),
+    port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
     tls: {}
 });
